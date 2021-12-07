88095e32c478be7f5276701aafb8c0bbf5ee7bb2
Julia Gatina <julia.sarandi@gmail.com>
2021-12-07 03:14:08 +0000

    Added a new Endpoint with /urls path, created urls_index file and added basic html skeleton to it

--- express_server.js
+++ express_server.js
@@ -15,15 +15,22 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// API (host: 'http://localhost:8080', method: 'GET', path: '/hello')
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
 // API (host: 'http://localhost:8080', method: 'GET', path: '/urls.json')
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-// API (host: 'http://localhost:8080', method: 'GET', path: '/hello')
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+// API (host: 'http://localhost:8080', method: 'GET', path: '/urls')
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
